7a99da5e8274cfc43b278a129cfd7d6f3c0acae7
ernesto905 <ernesto90643@gmail.com>
2024-08-26 19:02:38 -0400

    Lazy bootstrap module

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,84 +23,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Useful options
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
-vim.opt.breakindent = true
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Lines to keep above and below the cursor.
-vim.opt.scrolloff = 5
-
--- [[ Basic Keymaps ]]
-
--- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Disable arrow keys
-vim.keymap.set('n', '<left>', '<cmd>echo "Aww, widdle baby can\'t move?!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Aww, widdle baby can\'t move?!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Aww, widdle baby can\'t move?!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Aww, widdle baby can\'t move?!!"<CR>')
-
--- Make split navigation easier.
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- [[ Basic Autocommands ]]
-
--- Highlight when yanking
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- [[ Install `lazy.nvim` plugin manager ]]
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+require 'keymaps'
+require 'options'
+require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 require('lazy').setup({
@@ -790,9 +715,6 @@ require('lazy').setup({
     },
   },
 })
-
-require 'keymaps'
-require 'options'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
